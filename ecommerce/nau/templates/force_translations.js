adbb9551393b4924341c1cf738cdb3ecbec5b656
Ivo Branco <ivo.branco@fccn.pt>
2024-07-17 18:10:02 +0100

    feat(ecommerce): add js force translations
    Add JS force translations for ecommerce.

---
+++ ecommerce/nau/templates/force_translations.js
@@ -0,0 +1,11 @@
+// Translators: Original on ecommerce/static/templates/_offer_course_list.html
+gettext("Enroll Now")
+
+// Translators: Original on ecommerce/static/js/views/offer_view.js
+gettext("Course starts: %s")
+
+// Translators: Original on ecommerce/static/js/views/offer_view.js
+gettext("Discount valid until %s")
+
+// Translators: Original on: ecommerce/static/templates/_offer_course_list.html
+gettext('off')
